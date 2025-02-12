8d88b7296d70ae629a3714daaa10063a83ec5a17
RitickLath <ritick943lath@gmail.com>
2025-02-12 16:32:06 +0530

    Lazy Loading on Palettes and Gradient

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Routes/Home";
 import ComponentLibrary from "./Routes/ComponentLibrary";
-import Gradient from "./Routes/Gradient";
+const Gradient = React.lazy(() => import("./Routes/Gradient"));
+const Palettes = React.lazy(() => import("./Routes/Palettes"));
 import Templates from "./Routes/Templates";
 import Navbar from "./Components/Navbar";
-import Palettes from "./Routes/Palettes";
 import TextGradient from "./Routes/TextGradient";
 import CTA from "./Components/CTA";
 
@@ -23,10 +24,24 @@ const App = () => {
           }
         />
         <Route path="/components" element={<ComponentLibrary />} />
-        <Route path="/gradients" element={<Gradient />} />
+        <Route
+          path="/gradients"
+          element={
+            <React.Suspense fallback={<div>Loading</div>}>
+              <Gradient />
+            </React.Suspense>
+          }
+        />
         <Route path="/templates" element={<Templates />} />
         <Route path="/text-gradient" element={<TextGradient />} />
-        <Route path="/Palettes" element={<Palettes />} />
+        <Route
+          path="/Palettes"
+          element={
+            <React.Suspense fallback={<div>Loading</div>}>
+              <Palettes />
+            </React.Suspense>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
